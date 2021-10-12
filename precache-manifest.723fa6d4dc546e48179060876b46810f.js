e11e1b346d5d8bc6d79dd388f67d9f368dd5aad3
Wallace <wallaceedua@gmail.com>
2021-10-12 17:58:26 -0300

    Updates

---
+++ precache-manifest.723fa6d4dc546e48179060876b46810f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e59011ae61154a26a31c71a7c1fe26d8",
+    "url": "/Application-FDTE/index.html"
+  },
+  {
+    "revision": "666883979c39407e2f5f",
+    "url": "/Application-FDTE/static/js/2.dafa4c4e.chunk.js"
+  },
+  {
+    "revision": "dad561af0568c79a501381148643f9d1",
+    "url": "/Application-FDTE/static/js/2.dafa4c4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b81a333ed6143bee4ec",
+    "url": "/Application-FDTE/static/js/main.0bdd3df5.chunk.js"
+  },
+  {
+    "revision": "6d0a3f47ba1aba6a16e7",
+    "url": "/Application-FDTE/static/js/runtime-main.792ab882.js"
+  },
+  {
+    "revision": "23e6cf33e699e89ed95e77baa09cc197",
+    "url": "/Application-FDTE/static/media/GeorgiaPro-CondRegular.23e6cf33.ttf"
+  },
+  {
+    "revision": "b63e0e9bc629601dada46609083b63d1",
+    "url": "/Application-FDTE/static/media/discount.b63e0e9b.svg"
+  },
+  {
+    "revision": "ca8446d58eb8315c7a458caf81c6ed86",
+    "url": "/Application-FDTE/static/media/georgia.ca8446d5.ttf"
+  }
+]);
